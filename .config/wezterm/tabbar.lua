40a1648a54104283921e3dfb8735b5aab3a23be5
Stefan Schwaighofer <ste.schwaighofer@gmail.com>
2024-05-06 11:58:33 +0200

    build(nvim): update plugins

--- .config/wezterm/tabbar.lua
+++ .config/wezterm/tabbar.lua
@@ -17,8 +17,6 @@ local M = {}
 
 function M.setup()
 	wezterm.on("format-tab-title", function(tab, _, panes, _, hover)
-		local title = tab_title(tab)
-
 		local bg = Palette.tabbar.inactive_bg
 		local fg = Palette.tabbar.inactive_fg
 
@@ -37,12 +35,11 @@ function M.setup()
 
 		if hover then
 			table.insert(items, { Attribute = { Intensity = "Bold" } })
-			table.insert(items, { Attribute = { Italic = false } })
 		end
 
 		table.insert(items, { Background = { Color = bg } })
 		table.insert(items, { Foreground = { Color = fg } })
-		table.insert(items, { Text = string.format(" %d %s ", tab.tab_index + 1, title) })
+		table.insert(items, { Text = string.format(" %d %s ", tab.tab_index + 1, tab_title(tab)) })
 
 		if tab.is_active and #panes > 1 then
 			local panes_text = ""
