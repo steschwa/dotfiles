bb2b5020b2c7a40c462c4cd74b9bc10386783aa5
Stefan Schwaighofer <ste.schwaighofer@gmail.com>
2024-05-30 18:05:49 +0200

    style(wezterm): remove newlines

--- .config/wezterm/keybinds.lua
+++ .config/wezterm/keybinds.lua
@@ -31,77 +31,25 @@ function M.setup(config)
 		{ key = "e", mods = "OPT", action = act.SendString("€") },
 
 		-- font size
-		{
-			key = "-",
-			mods = "CMD",
-			action = act.DecreaseFontSize,
-		},
-		{
-			key = "+",
-			mods = "CMD",
-			action = act.IncreaseFontSize,
-		},
+		{ key = "-", mods = "CMD", action = act.DecreaseFontSize },
+		{ key = "+", mods = "CMD", action = act.IncreaseFontSize },
 
 		-- panes
-		{
-			key = "d",
-			mods = "CMD",
-			action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }),
-		},
-		{
-			key = "[",
-			mods = "CMD",
-			action = act.ActivatePaneDirection("Left"),
-		},
-		{
-			key = "]",
-			mods = "CMD",
-			action = act.ActivatePaneDirection("Right"),
-		},
-		{
-			key = "=",
-			mods = "CMD|SHIFT",
-			action = act.AdjustPaneSize({ "Left", 10 }),
-		},
-		{
-			key = "-",
-			mods = "CMD|SHIFT",
-			action = act.AdjustPaneSize({ "Right", 10 }),
-		},
-		{
-			key = "w",
-			mods = "CMD",
-			action = act.CloseCurrentPane({ confirm = true }),
-		},
+		{ key = "d", mods = "CMD", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+		{ key = "n", mods = "CMD", action = act.ActivatePaneDirection("Left") },
+		{ key = "p", mods = "CMD", action = act.ActivatePaneDirection("Right") },
+		{ key = "=", mods = "CMD|SHIFT", action = act.AdjustPaneSize({ "Left", 10 }) },
+		{ key = "-", mods = "CMD|SHIFT", action = act.AdjustPaneSize({ "Right", 10 }) },
+		{ key = "w", mods = "CMD", action = act.CloseCurrentPane({ confirm = true }) },
 
 		-- tabs
-		{
-			key = "LeftArrow",
-			mods = "CMD",
-			action = act.MoveTabRelative(-1),
-		},
-		{
-			key = "RightArrow",
-			mods = "CMD",
-			action = act.MoveTabRelative(1),
-		},
-		{
-			key = "h",
-			mods = "CMD|SHIFT",
-			action = act.ActivateTabRelative(-1),
-		},
-		{
-			key = "l",
-			mods = "CMD|SHIFT",
-			action = act.ActivateTabRelative(1),
-		},
+		{ key = "LeftArrow", mods = "CMD", action = act.MoveTabRelative(-1) },
+		{ key = "RightArrow", mods = "CMD", action = act.MoveTabRelative(1) },
+		{ key = "h", mods = "CMD|SHIFT", action = act.ActivateTabRelative(-1) },
+		{ key = "l", mods = "CMD|SHIFT", action = act.ActivateTabRelative(1) },
 
 		-- copy mode
-		{
-			key = "c",
-			mods = "LEADER",
-			action = act.ActivateCopyMode,
-		},
+		{ key = "c", mods = "LEADER", action = act.ActivateCopyMode },
 
 		-- TODO: removeme once `config.disable_default_key_bindings = true` is used
 		{
