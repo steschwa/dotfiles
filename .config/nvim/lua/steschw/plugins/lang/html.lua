1f97775063ec19d86e7a9e39e6fdc0821573b663
Stefan Schwaighofer <ste.schwaighofer@gmail.com>
2024-06-01 10:12:50 +0200

    feat(nvim): html lsp

--- .config/nvim/lua/steschw/plugins/lang/html.lua
+++ .config/nvim/lua/steschw/plugins/lang/html.lua
@@ -1,5 +1,14 @@
 local deps = require("steschw.utils.dependencies")
 
 return {
+    {
+        "neovim/nvim-lspconfig",
+        dependencies = {
+            deps.mason("html-lsp"),
+        },
+        opts = function(_, opts)
+            opts.servers.html = {}
+        end,
+    },
     deps.formatter("prettierd", { "html" }),
 }
