8bfd68303b964b1ec0c7358470c9b20b81b7f4e0
Stefan Schwaighofer <ste.schwaighofer@gmail.com>
2024-06-13 08:56:07 +0200

    fix(nvim): set nushell config paths for lsp

--- .config/nvim/lua/steschw/plugins/lang/nu.lua
+++ .config/nvim/lua/steschw/plugins/lang/nu.lua
@@ -2,7 +2,16 @@ return {
     {
         "neovim/nvim-lspconfig",
         opts = function(_, opts)
-            opts.servers.nushell = {}
+            opts.servers.nushell = {
+                cmd = {
+                    "/opt/homebrew/bin/nu",
+                    "--config",
+                    "~/.config/nushell/config.nu",
+                    "--env-config",
+                    "~/.config/nushell/env.nu",
+                    "--lsp",
+                },
+            }
         end,
     },
 }
