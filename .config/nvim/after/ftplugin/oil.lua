15e51131dec4f75840f102276777d8e0e7f910c4
Stefan Schwaighofer <ste.schwaighofer@gmail.com>
2024-10-21 13:40:56 +0200

    refactor(nvim): remove unused keymaps

--- .config/nvim/after/ftplugin/oil.lua
+++ .config/nvim/after/ftplugin/oil.lua
@@ -11,17 +11,3 @@ keymap("n", "-", act.parent.callback, keymap_opts)
 keymap("n", "^", act.open_cwd.callback, keymap_opts)
 keymap("n", "g.", act.toggle_hidden.callback, keymap_opts)
 keymap("n", "<C-r>", act.refresh.callback, keymap_opts)
-keymap("n", "<C-p>", act.preview.callback, keymap_opts)
-
-keymap("n", "==", function()
-    local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
-
-    local line_width = 1
-    for _, line in ipairs(lines) do
-        line_width = math.max(line_width, vim.fn.strdisplaywidth(line))
-    end
-
-    line_width = math.max(line_width, 40)
-
-    vim.api.nvim_win_set_width(0, line_width + 1)
-end, keymap_opts)
