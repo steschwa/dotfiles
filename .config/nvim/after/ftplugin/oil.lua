975bbb0dcc38e88fc986ab3f7c3cd5a128c9122c
Stefan Schwaighofer <ste.schwaighofer@gmail.com>
2024-12-17 11:45:58 +0100

    feat: close oil on escape

--- .config/nvim/after/ftplugin/oil.lua
+++ .config/nvim/after/ftplugin/oil.lua
@@ -5,8 +5,8 @@ local act = require("oil.actions")
 local keymap_opts = { buffer = 0 }
 
 keymap("n", "<cr>", act.select.callback, keymap_opts)
-keymap("n", "<C-s>", act.select_vsplit.callback, keymap_opts)
 keymap("n", "q", act.close.callback, keymap_opts)
+keymap("n", "<esc>", act.close.callback, keymap_opts)
 keymap("n", "-", act.parent.callback, keymap_opts)
 keymap("n", "^", act.open_cwd.callback, keymap_opts)
 keymap("n", "g.", act.toggle_hidden.callback, keymap_opts)
