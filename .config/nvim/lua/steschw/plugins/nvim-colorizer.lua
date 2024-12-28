d9e81881b61750d78e1dadb6ef1fecdd3a3bbf1f
Stefan Schwaighofer <ste.schwaighofer@gmail.com>
2024-12-28 10:17:41 +0100

    build(nvim): update plugins

--- .config/nvim/lua/steschw/plugins/nvim-colorizer.lua
+++ .config/nvim/lua/steschw/plugins/nvim-colorizer.lua
@@ -2,11 +2,10 @@ local filetypes = {
     "css",
     "scss",
     "dump",
-    "go",
 }
 
 return {
-    "NvChad/nvim-colorizer.lua",
+    "catgoose/nvim-colorizer.lua",
     event = { "BufReadPre", "BufNewFile" },
     filetypes = filetypes,
     config = function()
@@ -15,9 +14,6 @@ return {
             user_default_options = {
                 mode = "virtualtext",
                 virtualtext = "■■",
-                RGB = true, -- #RGB hex codes
-                RRGGBB = true, -- #RRGGBB hex codes
-                names = true, -- "Name" codes like Blue or blue
                 css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
             },
         })
