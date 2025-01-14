aa27c2810a5351fa9d111df585dccc6f82ee8cb6
Stefan Schwaighofer <ste.schwaighofer@gmail.com>
2025-01-14 08:59:49 +0100

    refactor(nvim): remove project-local snippet

--- .config/nvim/lua/steschw/plugins/snippets/typescript/init.lua
+++ .config/nvim/lua/steschw/plugins/snippets/typescript/init.lua
@@ -1,6 +1,5 @@
 return {
     require("steschw.plugins.snippets.typescript.shook"),
     require("steschw.plugins.snippets.typescript.imp"),
-    require("steschw.plugins.snippets.typescript.smigration"),
     require("steschw.plugins.snippets.typescript.log"),
 }
