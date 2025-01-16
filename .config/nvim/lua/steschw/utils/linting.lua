e38362433359b7d9fbf28094a1fc1a9bf858e165
Stefan Schwaighofer <ste.schwaighofer@gmail.com>
2025-01-16 21:15:18 +0100

    fix(nvim): organize python imports

--- .config/nvim/lua/steschw/utils/linting.lua
+++ .config/nvim/lua/steschw/utils/linting.lua
@@ -8,9 +8,30 @@ function M.lint()
     end)
 end
 
+local function fix_eslint()
+    vim.cmd.EslintFixAll()
+end
+
+local function fix_pyright()
+    local bufnr = vim.api.nvim_get_current_buf()
+    local clients = vim.lsp.get_clients({
+        bufnr = bufnr,
+        name = "pyright",
+    })
+    if #clients == 0 then
+        return
+    end
+
+    local client = clients[1]
+    client.request_sync("workspace/executeCommand", {
+        command = "pyright.organizeimports",
+        arguments = { vim.uri_from_bufnr(0) },
+    }, 500, bufnr)
+end
+
 function M.fix()
-    pcall(vim.cmd.EslintFixAll)
-    pcall(vim.cmd.PyrightOrganizeImports)
+    pcall(fix_eslint)
+    pcall(fix_pyright)
 end
 
 return M
