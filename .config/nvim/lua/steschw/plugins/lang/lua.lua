f6096443a937c5aee94a1147502a7ecb7e2a8aba
Stefan Schwaighofer <ste.schwaighofer@gmail.com>
2025-01-27 20:15:30 +0100

    feat(nvim): luals types for custom plugins

--- .config/nvim/lua/steschw/plugins/lang/lua.lua
+++ .config/nvim/lua/steschw/plugins/lang/lua.lua
@@ -2,22 +2,9 @@
 
 return {
     {
-        "folke/lazydev.nvim",
-        ft = "lua",
-        config = true,
-    },
-    {
         "neovim/nvim-lspconfig",
         opts = function(_, opts)
-            opts.servers.lua_ls = {
-                settings = {
-                    Lua = {
-                        runtime = {
-                            version = "LuaJIT",
-                        },
-                    },
-                },
-            }
+            opts.servers.lua_ls = {}
         end,
     },
 }
