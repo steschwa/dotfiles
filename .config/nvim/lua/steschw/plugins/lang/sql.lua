36fb70849c15d38906ecaa4e8ebda96a3fc682bc
Stefan Schwaighofer <ste.schwaighofer@gmail.com>
2025-03-28 11:00:52 +0100

    feat(nvim): use pgtools as lsp

--- .config/nvim/lua/steschw/plugins/lang/sql.lua
+++ .config/nvim/lua/steschw/plugins/lang/sql.lua
@@ -1,10 +1,10 @@
--- MasonInstall sqls
+-- MasonInstall postgrestools
 
 return {
     {
         "neovim/nvim-lspconfig",
         opts = function(_, opts)
-            opts.servers.sqls = {}
+            opts.servers.postgres_lsp = {}
         end,
     },
 }
