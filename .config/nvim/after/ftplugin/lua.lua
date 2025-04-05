b0231296bf2ce44bba73201abca25547e75c85a3
Stefan Schwaighofer <ste.schwaighofer@gmail.com>
2025-04-05 11:50:29 +0200

    fix(nvim): keymap lua buffer only

--- .config/nvim/after/ftplugin/lua.lua
+++ .config/nvim/after/ftplugin/lua.lua
@@ -3,10 +3,10 @@ local keymap = require("steschw.utils.keys").keymap
 keymap("n", "?", function()
     local word_under_cursor = vim.fn.expand("<cword>")
 
-    local ok = pcall(function()
-        vim.cmd("h " .. word_under_cursor)
-    end)
+    local ok = pcall(vim.cmd.h, word_under_cursor)
     if not ok then
         vim.print("no help entry for " .. word_under_cursor)
     end
-end)
+end, {
+    buffer = 0,
+})
