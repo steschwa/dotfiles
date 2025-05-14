bf2eef1785382936d6cfbc230444162b58236644
Stefan Schwaighofer <ste.schwaighofer@gmail.com>
2025-05-14 15:41:50 +0200

    feat(nvim): reduce blink documentation scroll

--- .config/nvim/lua/steschw/plugins/blink.lua
+++ .config/nvim/lua/steschw/plugins/blink.lua
@@ -69,8 +69,18 @@ return {
             ["<C-p>"] = { "select_prev", "fallback_to_mappings" },
             ["<C-n>"] = { "select_next", "fallback_to_mappings" },
 
-            ["<C-b>"] = { "scroll_documentation_up", "fallback" },
-            ["<C-f>"] = { "scroll_documentation_down", "fallback" },
+            ["<C-u>"] = {
+                function(cmp)
+                    cmp.scroll_documentation_up(2)
+                end,
+                "fallback",
+            },
+            ["<C-d>"] = {
+                function(cmp)
+                    cmp.scroll_documentation_down(2)
+                end,
+                "fallback",
+            },
         },
     },
 }
