1feb2570d0288f9b3a6d28bbe5f2de1accfe64e3
Stefan Schwaighofer <ste.schwaighofer@gmail.com>
2025-05-19 08:35:24 +0200

    feat(nvim): visually debug hl groups command

--- .config/nvim/lua/steschw/config/commands.lua
+++ .config/nvim/lua/steschw/config/commands.lua
@@ -14,3 +14,28 @@ end, {
     nargs = 1,
     complete = "command",
 })
+
+vim.api.nvim_create_user_command("DumpHl", function()
+    local hls = vim.api.nvim_get_hl(0, {})
+
+    local bufid = vim.api.nvim_create_buf(false, true)
+    local ns = vim.api.nvim_create_namespace("dump-hl")
+
+    local line_nr = 0
+
+    for name, hl in pairs(hls) do
+        local bg = hl.bg and string.format("#%6x", hl.bg) or "NONE"
+        local fg = hl.fg and string.format("#%6x", hl.fg) or "NONE"
+
+        local line = string.format("%-40s bg=%7s fg=%7s link=%s", name, bg, fg, hl.link or "NONE")
+
+        vim.api.nvim_buf_set_lines(bufid, line_nr, line_nr, false, { line })
+        vim.hl.range(bufid, ns, name, { line_nr, 0 }, { line_nr, #name })
+    end
+
+    vim.cmd("vsplit")
+    vim.api.nvim_set_current_buf(bufid)
+end, {
+    force = false,
+    nargs = 0,
+})
