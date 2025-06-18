ac3acada860df59644212d474d5a843e53c55604
Stefan Schwaighofer <ste.schwaighofer@gmail.com>
2025-06-18 15:15:29 +0200

    feat(nvim): no lazy autocmds

--- .config/nvim/lua/steschw/init.lua
+++ .config/nvim/lua/steschw/init.lua
@@ -2,7 +2,8 @@ vim.api.nvim_create_autocmd("User", {
     pattern = "VeryLazy",
     callback = function()
         require("steschw.config.keymaps")
-        require("steschw.config.autocmds")
         require("steschw.config.commands")
     end,
 })
+
+require("steschw.config.autocmds")
