3bc23a9910676cf5a4b28dc78832f2ae265791b0
Stefan Schwaighofer <ste.schwaighofer@gmail.com>
2025-07-13 13:54:35 +0200

    refactor(nvim): run one formatter only

--- .config/nvim/lua/steschw/plugins/conform.lua
+++ .config/nvim/lua/steschw/plugins/conform.lua
@@ -7,39 +7,26 @@ return {
         conform.setup({
             notify_on_error = false,
             formatters_by_ft = {
-                css = { "biome-check", "prettier", stop_after_first = true },
+                css = { "biome-check", "prettier" },
                 go = { "golangci-lint" },
-                graphql = { "biome-check", "prettier", stop_after_first = true },
+                graphql = { "biome-check", "prettier" },
                 html = { "prettier" },
-                javascript = { "biome-check", "prettier", stop_after_first = true },
-                javascriptreact = { "biome-check", "prettier", stop_after_first = true },
-                json = { "biome-check", "prettier", stop_after_first = true },
-                jsonc = { "biome-check", "prettier", stop_after_first = true },
+                javascript = { "biome-check", "prettier" },
+                javascriptreact = { "biome-check", "prettier" },
+                json = { "biome-check", "prettier" },
+                jsonc = { "biome-check", "prettier" },
                 lua = { "stylua" },
                 markdown = { "prettier" },
                 python = { "ruff_format" },
                 scss = { "prettier" },
                 sql = { "sqruff" },
-                typescript = { "biome-check", "prettier", stop_after_first = true },
-                typescriptreact = { "biome-check", "prettier", stop_after_first = true },
+                typescript = { "biome-check", "prettier" },
+                typescriptreact = { "biome-check", "prettier" },
                 yaml = { "prettier" },
-            },
-            formatters = {
-                ["biome-check"] = {
-                    require_cwd = true,
-                },
-                ["bibtex-tidy"] = {
-                    prepend_args = {
-                        "--numeric",
-                        "--space=4",
-                        "--blank-lines",
-                        "--strip-enclosing-braces",
-                        "--trailing-commas",
-                    },
-                },
             },
             default_format_opts = {
                 lsp_format = "fallback",
+                stop_after_first = true,
             },
         })
     end,
