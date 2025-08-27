f553ec49c901bf292420f90f4dbb47bbc6ffb2fd
Stefan Schwaighofer <ste.schwaighofer@gmail.com>
2025-08-27 20:32:09 +0200

    refactor(nvim): function readability

--- .config/nvim/after/lsp/ts_ls.lua
+++ .config/nvim/after/lsp/ts_ls.lua
@@ -1,19 +1,23 @@
+local keymap = require("steschw.utils.keys").keymap
+
+local function remove_unused_imports()
+    vim.lsp.buf.code_action({
+        context = {
+            diagnostics = {},
+            ---@diagnostic disable-next-line: assign-type-mismatch
+            only = { "source.removeUnusedImports.ts" },
+        },
+        filter = function(action)
+            return action.kind == "source.removeUnusedImports.ts"
+        end,
+        apply = true,
+    })
+end
+
 return {
     ---@param _ vim.lsp.Client
     ---@param bufnr number
     on_attach = function(_, bufnr)
-        require("steschw.utils.keys").keymap("n", "gu", function()
-            vim.lsp.buf.code_action({
-                context = {
-                    diagnostics = {},
-                    ---@diagnostic disable-next-line: assign-type-mismatch
-                    only = { "source.removeUnusedImports.ts" },
-                },
-                filter = function(action)
-                    return action.kind == "source.removeUnusedImports.ts"
-                end,
-                apply = true,
-            })
-        end, { buffer = bufnr })
+        keymap("n", "gu", remove_unused_imports, { buffer = bufnr })
     end,
 }
