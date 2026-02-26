13e7e9956c034a9511affa922763d0ebd171ff2d
Stefan Schwaighofer <ste.schwaighofer@gmail.com>
2026-02-26 16:17:39 +0100

    feat(kitty): show active session

--- .config/kitty/tab_bar.py
+++ .config/kitty/tab_bar.py
@@ -1,18 +1,53 @@
 # pyright: reportMissingImports=false
 import json
 
-from kitty.fast_data_types import get_boss
-from kitty.tab_bar import as_rgb, draw_title
+from kitty.fast_data_types import Screen, get_boss, get_options
+from kitty.tab_bar import TabBarData, as_rgb, draw_title
 from kitty.utils import color_as_int
 
 INDICATOR_NORMAL = "○"
 INDICATOR_OVERLAY = "●"
 
+opts = get_options()
+
+
+def get_session_name(tab: TabBarData, max_length: int) -> str | None:
+    text = tab.session_name
+    if not text:
+        return None
+
+    if len(text) <= max_length:
+        return text
+    elif max_length >= 3:
+        return text[:3]
+
+    return None
+
+
+def draw_right_status(screen: Screen, tab: TabBarData) -> int:
+    max_length = screen.columns - screen.cursor.x
+    text = get_session_name(tab, max_length)
+    print("text:", text)
+    if text is None:
+        return screen.cursor.x
+
+    spaces = screen.columns - screen.cursor.x - len(text)
+    if spaces > 0:
+        screen.draw(" " * spaces)
+
+    screen.cursor.fg = as_rgb(color_as_int(opts.inactive_tab_foreground))
+    screen.draw(text)
+
+    if screen.columns - screen.cursor.x > len(text):
+        screen.cursor.x = screen.columns - len(text)
+
+    return screen.cursor.x
+
 
 def draw_tab(
     draw_data,
-    screen,
-    tab,
+    screen: Screen,
+    tab: TabBarData,
     __before__: int,
     max_tab_length: int,
     index: int,
@@ -33,13 +68,16 @@ def draw_tab(
     if draw_win_indicator:
         indicators = list(
             map(
-                lambda group: INDICATOR_OVERLAY
-                if len(group["windows"]) > 1
-                else INDICATOR_NORMAL,
+                lambda group: (
+                    INDICATOR_OVERLAY if len(group["windows"]) > 1 else INDICATOR_NORMAL
+                ),
                 win_groups,
             )
         )
         screen.draw(" " + "".join(indicators))
+
+    if is_last:
+        draw_right_status(screen, tab)
 
     if not is_last:
         screen.cursor.bg = as_rgb(color_as_int(draw_data.inactive_bg))
