ed9d2ceec2369c81cfc8df0dc8324e0f59470849
Stefan Schwaighofer <ste.schwaighofer@gmail.com>
2026-02-27 14:07:50 +0100

    feat(kitty): session to tabs watcher

---
+++ .config/kitty/watchers/session.py
@@ -0,0 +1,25 @@
+import json
+import os
+from typing import Any
+
+from kitty.boss import Boss
+from kitty.window import Window
+
+
+def on_tab_bar_dirty(boss: Boss, window: Window, data: dict[str, Any]) -> None:
+    session_to_tabs = {}
+
+    for tab in boss.all_tabs:
+        session = tab.active_session_name or "default"
+
+        tab_names = session_to_tabs[session] if session in session_to_tabs else []
+        tab_names.append(tab.effective_title)
+
+        session_to_tabs[session] = tab_names
+
+    pid = os.environ.get("KITTY_PID")
+    if pid is None:
+        return
+
+    with open(f"/tmp/kitty-{pid}-sessions.json", "w") as file:
+        json.dump(session_to_tabs, file)
