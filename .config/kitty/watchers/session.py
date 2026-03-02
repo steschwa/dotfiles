4bc3df161b99a83fa2f7801f9eea523849e228de
Stefan Schwaighofer <ste.schwaighofer@gmail.com>
2026-03-02 12:09:57 +0100

    Revert "chore(kitty): revert session usage"
    This reverts commit e591a2c3747ad92c14ba3e848ba2883c716c91a9.

---
+++ .config/kitty/watchers/session.py
@@ -0,0 +1,28 @@
+import json
+from typing import Any
+
+from kitty.boss import Boss
+from kitty.child import getpid
+from kitty.window import Window
+
+
+def on_tab_bar_dirty(boss: Boss, window: Window, data: dict[str, Any]) -> None:
+    session_to_tabs = {}
+
+    for tab in boss.all_tabs:
+        session = tab.active_session_name or "default"
+
+        tab_names = session_to_tabs[session] if session in session_to_tabs else []
+        tab_names.append(tab.effective_title)
+
+        session_to_tabs[session] = tab_names
+
+    pid = getpid()
+    with open(f"/tmp/kitty-{pid}-sessions.json", "w") as file:
+        json.dump(
+            {
+                "active_session": boss.active_session,
+                "sessions": session_to_tabs,
+            },
+            file,
+        )
