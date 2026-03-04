805a3be7855e742366218c3f80cfe721b0e7c1e1
Stefan Schwaighofer <ste.schwaighofer@gmail.com>
2026-03-04 14:07:54 +0100

    feat(kitty): none session name

--- .config/kitty/watchers/session.py
+++ .config/kitty/watchers/session.py
@@ -5,12 +5,16 @@ from kitty.boss import Boss
 from kitty.child import getpid
 from kitty.window import Window
 
+NONE_SESSION_NAME = "<none>"
+
 
 def on_tab_bar_dirty(boss: Boss, window: Window, data: dict[str, Any]) -> None:
     session_to_tabs = {}
 
     for tab in boss.all_tabs:
-        session = tab.active_session_name or "default"
+        session = (
+            tab.active_session_name or tab.created_in_session_name or NONE_SESSION_NAME
+        )
 
         tab_names = session_to_tabs[session] if session in session_to_tabs else []
         tab_names.append(tab.effective_title)
@@ -21,8 +25,16 @@ def on_tab_bar_dirty(boss: Boss, window: Window, data: dict[str, Any]) -> None:
     with open(f"/tmp/kitty-{pid}-sessions.json", "w") as file:
         json.dump(
             {
-                "active_session": boss.active_session,
+                "active_session": boss.active_session or NONE_SESSION_NAME,
                 "sessions": session_to_tabs,
+                "tabs": boss.active_tab_manager
+                and list(
+                    map(
+                        lambda t: t.effective_title,
+                        boss.active_tab_manager.tabs_to_be_shown_in_tab_bar,
+                    )
+                )
+                or [],
             },
             file,
         )
